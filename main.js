a2c3ab4565a4f72f644d8492f9d9e47341ef446d
maha276 <mahaforo276@gmail.com>
2019-03-27 10:42:39 +0200

    fixed api link

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 // Callback for the function to have a click button
-document.getElementById("button").addEventListener("click", myFunction);
-// Create a function to 
-function myFunction () {
-  fetch("https://api.themoviedb.org/3/search/movie?api_key=3bbdbed2c291857dbe65e935c9dea7ed")
+// Create a function to
+function getMovieData (event) {
+  event.preventDefault();
+  var searchValue = document.getElementById("search").value;
+
+  fetch("https://api.themoviedb.org/3/search/movie?api_key=3bbdbed2c291857dbe65e935c9dea7ed&language=en-US&query=" + searchValue + "&page=1&include_adult=false")
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
+      //document.getElementById("title").textContent = data.title;
+      // var a = document.createElement('a');
+      // var linkText = document.createTextNode("title");
+      // a.appendChild(linkText);
+      // a.title = "my title text";
+      // a.href = "http://example.com";
+      // document.body.appendChild(a);
 
+      console.log(data);
+    })
+    .catch(function(error) {
+      console.log(error);
     })
 }
+
+document.getElementById("button").addEventListener("click", getMovieData);
