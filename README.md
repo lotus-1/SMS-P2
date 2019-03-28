558a26fd306262268e367ad31f0ec879ecdb6166
sabeel29 <sabeel1999.29@gmail.com>
2019-03-28 11:05:58 +0200

    making sure that the code will orginaize

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ https://lotus-1.github.io/SMS-P2/
 <br>
 =======
 ###### Project week-2
->>>>>>> ba0975ed70a8651e5d65bae9e9d6a8ced9166d2e
+
 
 # OUR TEAM:
 
@@ -18,13 +18,12 @@ https://lotus-1.github.io/SMS-P2/
 <br>
 
 We built our application to let the user search for their favourite movies and TV-shows. The user can get information about the movie, like the release date, movie summary and rate. :cinema:
----
+
 <br>
 We used API, JAVASCRIPT, CSS, HTML5 technologies to build our app<br>
----
+
 <br>
 Some of the challenges we encountered are, finding the API key, finding the image URL..
----
+
 <br>
 If we were going to continue working on this application, we would add a trailer to the movie, and more search options, like searching for actors.
----
--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> ba0975ed70a8651e5d65bae9e9d6a8ced9166d2e
 // We create a var to hold the constant url .
 var imgUrl = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/";
 
@@ -17,8 +14,8 @@ function getMovieData (event) {
     })
 
     .then(function(data) {
-      var result1 = data.results[0] ;
-      console.log(result1.poster_path);
+      var result1 = data.results[0];
+
       document.getElementById("title1").textContent = result1.title;
       document.getElementById("image1").src = imgUrl + result1.poster_path;
       document.getElementById("date1").textContent = result1.release_date;
@@ -27,23 +24,7 @@ function getMovieData (event) {
 
       var result2 = data.results[1] ;
 
-      console.log(result2.poster_path);
       document.getElementById("title2").textContent = result2.title;
-<<<<<<< HEAD
-    document.getElementById("image2").src = imgUrl + result2.poster_path;
-    document.getElementById("date2").textContent = result2.release_date;
-    document.getElementById("summary2").textContent = result2.overview;
-    document.getElementById("average2").textContent = result2.vote_average;
-
-    var result3 = data.results[2] ;
-
-    console.log(result3.poster_path);
-    document.getElementById("title3").textContent = result3.title;
-    document.getElementById("image3").src = imgUrl + result3.poster_path;
-    document.getElementById("date3").textContent = result3.release_date;
-    document.getElementById("summary3").textContent = result3.overview;
-    document.getElementById("average3").textContent = result3.vote_average;
-=======
       document.getElementById("image2").src = imgUrl + result2.poster_path;
       document.getElementById("date2").textContent = result2.release_date;
       document.getElementById("summary2").textContent = result2.overview;
@@ -51,18 +32,17 @@ function getMovieData (event) {
 
       var result3 = data.results[2] ;
 
-      console.log(result3.poster_path);
       document.getElementById("title3").textContent = result3.title;
       document.getElementById("image3").src = imgUrl + result3.poster_path;
       document.getElementById("date3").textContent = result3.release_date;
       document.getElementById("summary3").textContent = result3.overview;
       document.getElementById("average3").textContent = result3.vote_average;
->>>>>>> ba0975ed70a8651e5d65bae9e9d6a8ced9166d2e
+
 })
     //console.log(data);
 
   .catch(function(error) {
-    //console.log(error);
+    console.log(error);
   })
 }
 // Adding a callback to have a click
